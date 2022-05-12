cba172dd2c5eba56f6314edf12e9d03bdad6c15b
ManuelHMR <manuelhmrjf@gmail.com>
2022-05-12 17:44:21 -0300

    total function cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,53 @@
 import styled from 'styled-components';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import {Link} from "react-router-dom";
 
 import CardProduct from './CardProduct';
 
 import CartContext from './../contexts/CartContext';
 
+const token = localStorage.getItem("token")
+
 function Cart() {
     const {cart} = useContext(CartContext);
+    const [validateToken, setValidateToken] = useState(false);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        if(token){
+            setValidateToken(true);
+        } else{
+            setValidateToken(false);
+        }
+    }, [])
+
+    useEffect( () =>{
+        let soma = 0
+        for(let i = 0; i < cart.length; i++){
+            soma += Number(cart[i].realPrice)
+        }
+        setTotal(soma.toFixed(2));
+    }, [cart]) 
+
+    function Button(){
+        if(!validateToken){
+            return(
+                <Link to={"/sign-in"}>
+                    <button>Faça o login!</button>
+                </Link>
+            )
+        }
+        if(validateToken){
+            return(
+                <button>Pagar</button>
+            )
+        }
+    };
 
     return(
         <>
         <Conteiner>
-            <Titulo>Carrinho</Titulo>
+            <Titulo>{validateToken? "Checkout" : "Carrinho"}</Titulo>
             <Space>
                 {cart.map((product) => {
                     const {image, name, type, price, realPrice, quantity} = product;
@@ -27,6 +63,11 @@ function Cart() {
                             />
                     );
                 })}
+                <div className='total'>
+                    <h1>Total</h1>
+                    <h1>R${total}</h1>
+                </div>
+                <Button></Button>
             </Space>
         </Conteiner>
         </>
@@ -44,6 +85,30 @@ const Conteiner = styled.main`
     color: #FFFFFF;
     @media (min-width: 800px){
         margin-top: 100px;
+    }
+    .total{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 351px;
+        height: 98px;
+        padding: 9px;
+        margin-bottom: 10px;
+        border-top: 2px dashed var(--boxColor);
+        border-bottom: 2px dashed var(--boxColor);
+        h1{
+            font-size: 25px;
+        }
+    }
+    button{
+        margin-top: 20px;
+        width: 298px;
+        height: 47px;
+        background-color: var(--buttonColor);
+        border: none;
+        border-radius: 10px;
+        color: var(--boxColor);
+        font-size: 24px;
     }
 `
 
